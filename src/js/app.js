c640c015684c3719374a3565cad92db94a9da35d
minsikim <minsiua@gmail.com>
2018-07-23 17:42:15 +0900

    scss babel loader

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import '../scss/main.scss';
+
 import * as functions from './component.js'
 
 console.log('hello');
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,43 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
-  },
-  devServer:{
-    contentBase: __dirname + '/public'
-  },
-  mode: 'development',
-}
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    devServer:{
+        contentBase: __dirname + '/dist'
+      },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin
+    ]
+};
